29b0f5807c582969d3561145c6baee1bb902326e
Simone Mac <simone.cattaneo@digitiamo.com>
2024-12-04 10:44:59 +0100

    ciao

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,6 @@ import { Landing } from './components/Landing';
 import { MouseFollower } from './components/parts/MouseFollower';
 
 export const App = () => {
-  // const page = useRef<HTMLDivElement>(null);
-  // const { contextSafe } = useGSAP(() => {}, { scope: page });
-
-  // const moveCircle: MouseEventHandler<HTMLDivElement> = contextSafe((e) => {
-  //   const tl = gsap.timeline();
-  //   tl.to('.circle', {
-  //     x: e.pageX,
-  //     y: e.pageY,
-  //     ease: 'elastic.in',
-  //   });
-  // });
-
-  // const stickCircle: MouseEventHandler<HTMLDivElement> = contextSafe((e) => {
-  //   const tl = gsap.timeline();
-
-  //   tl.set('.circle', {
-  //     x: e.pageX,
-  //     y: e.pageY,
-  //   });
-  // });
   return (
     <div className='w-full h-full relative'>
       <MouseFollower />
